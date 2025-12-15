f581a29faab17da3963958298be18323de27b3db
Redzone29 <ngruy0984@gmail.com>
2025-12-15 21:09:16 +0700

    up

---
+++ untitled/src/arrays/FindTheBiggestValue.java
@@ -0,0 +1,28 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class FindTheBiggestValue {
+    public static void main(String[] args) {
+        System.out.println("Type the size of array n x n: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("Enter the element at index (" + (i + 1) + "," + (j + 1) + "): ");
+                Scanner sc1 = new Scanner(System.in);
+                arr[i][j] = sc1.nextInt();
+            }
+        }
+        int max = arr[0][0];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        System.out.println("The biggest value is: " + max);
+    }
+}
