7f2f3e63e69fed4718070f234f2fe38c42ea0580
Redzone29 <ngruy0984@gmail.com>
2025-12-15 20:26:52 +0700

    up

---
+++ untitled/src/vongLap/hienThiCacSoNguyenToNhoHonMotTram.java
@@ -0,0 +1,18 @@
+package vongLap;
+
+public class hienThiCacSoNguyenToNhoHonMotTram {
+    public static void main(String[] args){
+        int count = 0;
+        for (int i = 2; i <= 100; i++){
+            count = 0;
+            for(int j = 1; j <= i; j++){
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count==2){
+                System.out.println(i);
+            }
+        }
+    }
+}
